5fb3837d0bea8a742c93cd16d3877938c63e0419
Muhammad Asim <mkhaskheli.bese18seecs@seecs.edu.pk>
2020-04-27 22:24:07 +0500

    Construct added

---
+++ BinaryTree.java
@@ -0,0 +1,12 @@
+class BinaryTree{
+    Node node;
+    static class Node{
+        int value;
+        Node left;
+        Node right;
+
+        Node(int value){
+            this.value=value;
+        }
+    }
+}
