fc2e976e9bb8224719afa5df9504c7f0f6a2a623
Muhammad Asim <mkhaskheli.bese18seecs@seecs.edu.pk>
2020-04-27 22:52:04 +0500

    Almost complete

--- BinaryTree.java
+++ BinaryTree.java
@@ -9,4 +9,37 @@ class BinaryTree{
             this.value=value;
         }
     }
-}
+
+    public static void Insert(Node node, int value){
+
+        if (node==null){
+            Node new_node=new Node(value);
+        }
+        
+       else if (value<new_node.value){
+            if (node.left!=null){
+                Insert(node.left,value);
+            
+            }
+            else{
+                node.left=new Node(value);
+            }
+            }
+
+        else if (value>node.value){
+            if (node.right!=null){
+                Insert(node.right,value);
+            }
+            else{
+                node.right=new Node(value);
+            }
+        }
+
+        }
+
+    
+        public static void main(String[] args){
+            BinaryTree tree= new BinaryTree();
+            tree.insert(new_node,5);
+        }
+    }
