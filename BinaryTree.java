052cf4bca9a84b13143aa9d02d493ef94c1f7afd
Muhammad Asim <mkhaskheli.bese18seecs@seecs.edu.pk>
2020-04-28 22:17:30 +0500

    Add and contains done

--- BinaryTree.java
+++ BinaryTree.java
@@ -1,7 +1,7 @@
 class BinaryTree{
     Node root;
     
-    public Node Insert(Node current, int value){
+    private Node Insert(Node current, int value){
         if (current==null){
          return new Node(value);
         }
@@ -16,18 +16,31 @@ class BinaryTree{
         else{
             return current;
         }
-
         return current;
-
         }
        
     public void add(int value){
         root=Insert(root,value);
     }
+
+    private boolean ContainsElement(Node current, int value){
+        if (current==null){
+            return false;
+        }
+        else if (current.value==value){
+            return true;
+        }
+        return value>current.value ? ContainsElement(current.right,value) : ContainsElement(current.left,value);
+    }
+
+    public boolean HasElement (int value){
+        return ContainsElement(root,value);
+    }
     public static void main(String[] args){
         BinaryTree tree= new BinaryTree();
         tree.add(5);
         tree.add(6);
+        tree.HasElement(5);
     }
         }
 
