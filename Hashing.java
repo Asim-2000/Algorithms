7a49c79232edbf5c2095f42628aa9aa44c6337f4
Muhammad Asim <mkhaskheli.bese18seecs@seecs.edu.pk>
2020-05-03 04:02:27 +0500

    Comments added

--- Hashing.java
+++ Hashing.java
@@ -1,5 +1,5 @@
 import java.util.ArrayList;
-
+// HashNode is a class in which we have three attributes a key (name or any other object), value( what is value? like in dictionary?) and a node that points to next obejct in the list.
 class HashNode<K,V>{
     K key;
     V value;
@@ -20,7 +20,9 @@ class Map <K,V>{
     private int size;
 
     public Map(){
-        
+        // bucket array is the array we have attached to the node of the hashmap to save items that have same key bcz multiple items can have the same key so we maintain//
+        // a linked lsit infronf of the node and iterate through it to retrive the correct obejct //
+       
         bucketArray=new ArrayList<>();
         numberofbuckets=10;
         size=0;
@@ -47,7 +49,7 @@ class Map <K,V>{
         HashNode<K,V> head =bucketArray.get(bucketIndex);
 
         HashNode<K,V> prev =null;
-        // iterate through the array list of buckets untill we reach the end of list 
+        // iterate through the array list of buckets untill we reach the end of list// 
         while (head!=null){
             if (head.key.equals(key)){
                 break;
@@ -59,18 +61,18 @@ class Map <K,V>{
         if (head==null){return null;}
 
         size--;
-        // if previous node is not null then we will point the previous node to the node on the next of head. 
+        // if previous node is not null then we will point the previous node to the node on the next of head. //
         if (prev!=null){
             prev.next=head.next;
         }
 
-        //else we will replace the object i.e node at the bucketIndex with the objcet at the next position.
+        //else we will replace the object i.e node at the bucketIndex with the objcet at the next position.//
         else{
             bucketArray.set(bucketIndex,head.next);
         }
         
         return head.value;
-
+        // Is the next element replicated? Check  //
 
 
         }        
