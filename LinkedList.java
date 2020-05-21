19537a19aee7d634e3cc2029fa83e9f9dabca715
Muhammad Asim <mkhaskheli.bese18seecs@seecs.edu.pk>
2020-05-21 22:47:19 +0500

    Reverse function added

--- LinkedList.java
+++ LinkedList.java
@@ -62,7 +62,20 @@ public class LinkedList{
             return list;
         }
 
-        public LinkedList Reverse (LinkedList lsit){
+        public Node Reverse (Node node){
+            Node prev=null;
+            Node current=node;
+            Node next=null;
+            while (current!=null){
+                next=current.next;
+                current.next=prev;
+                prev=current;
+                current=next;
+            }
+            node=prev;
+            return node;
+
+
             
         }
         
