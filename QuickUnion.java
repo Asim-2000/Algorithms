27985c316b16c686fe7c83c036289e45b9d4d46b
Muhammad Asim <mkhaskheli.bese18seecs@seecs.edu.pk>
2020-06-01 00:38:00 +0500

    Finished

--- QuickUnion.java
+++ QuickUnion.java
@@ -2,7 +2,7 @@ public class QuickUnion{
     public int[] id;
 
     public QuickUnion(int N){
-        id=new int[10];
+        id=new int[N];
         for (int i=0;i<N;i++){
             id[i]=i;
         }
