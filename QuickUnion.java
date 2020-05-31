0295c88e148a11e091e50f9e3adc32411212b723
Muhammad Asim <mkhaskheli.bese18seecs@seecs.edu.pk>
2020-06-01 00:23:50 +0500

    Problem in main, null pointer exception

--- QuickUnion.java
+++ QuickUnion.java
@@ -1,10 +1,11 @@
 public class QuickUnion{
     private int[] id;
 
-    public void QuickUnionQU(int N){
+    public int[] QuickUnionQU(int N){
         for (int i=0;i<N;i++){
             id[i]=i;
         }
+        return id;
     }
     private int root(int i){
         while(i!=id[i]){
@@ -23,4 +24,12 @@ public class QuickUnion{
         id[i]=j;
     }
 
+public static void main(String[] args){
+    QuickUnion U =new QuickUnion();
+    U.QuickUnionQU(10);
+    U.Union(5,6);
+    U.Union(7,8);
+    System.out.println(U.Connected(5,6));
+
+}
 }
