130c5a458044027f3d1f10683b096c69802d5641
Muhammad Asim <mkhaskheli.bese18seecs@seecs.edu.pk>
2020-06-03 00:39:21 +0500

    Percolation changed and now to continue

--- Percolation.java
+++ Percolation.java
@@ -4,32 +4,42 @@ import algs4.WeightedQuickUnionUF;
 public class Percolation{
     private boolean [][] grid;
     private int grid_size;
-    private int grid_squared;
-    private int virtual_top;
+    private int virtual_top=0;
     private int virtual_bottom;
     private int open_sites;
-    private WeightedQuickUnionUF wqfGrid;
-    private WeightedQuickUnionUF wqfFull;
+    private WeightedQuickUnionUF uf;
 
     // creates n-by-n grid, with all sites initially blocked
-    public Percolation(int n){
+    public Percolation(int N){
         if (n<=0){throw new IllegalArgumentException("N must be > 0");}
         grid_size=n;
-        grid_squared=n*n;
         grid = new boolean [grid_size][grid_size];
-        wqfGrid = new WeightedQuickUnionUF(grid_squared + 2); //including virtual top and bottom//
-        wqfFull = new WeightedQuickUnionUF(grid_squared + 1); //including virtual top//
-        virtual_bottom=grid_squared + 1;
-        virtual_top=grid_squared;
+        uf=new WeightedQuickUnion(N*N+2)
+        virtual_bottom=(N*N)+1;
         open_sites=0;    
 }
 
     // Test: open site (row, col) if it is not open already
     public void open(int row, int col) {
-        
+        grid[i-1][j-1]=true;
+
+        if (i==1){
+            uf.union(encoded_index(row,col),virtual_top);
+        }
+
+        if (i==grid_size){
+            uf.union(encoded_index(row,col),virtual_botom);
+        }
+
+        if ()
 
 
-    }
+}
+    //This medthod encodes the column and row(00,01,02....grid_size grid_size) in the matrix to a simple number from 1 to grid_size//
+    private int encoded_index(int row, int col){
+        return (grid_size*(i-1))+j
+    } 
+
 
 
 }
